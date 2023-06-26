d703760c16425e49736b45d7babcc00ce39da25a
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-06-26 10:30:37 -0300

    [M1S05] Ex 2 - Escreva um programa para tradução de cores

---
+++ exercicio2.js
@@ -0,0 +1,25 @@
+let cor = "esmeralda"; // Em inglês
+
+switch(cor) {
+  case "red": 
+    console.log("Vermelho");
+    break;
+  case "green":
+    console.log("Verde");
+    break;
+  case "blue":
+    console.log("Azul");
+    break;
+  case "yellow": 
+    console.log("Amarelo");
+    break;
+  case "black":
+    console.log("Preto");
+    break;
+  case "white":
+    console.log("Branco");
+    break;
+  default:
+    console.log("Cor não registrada");
+    break;
+}
