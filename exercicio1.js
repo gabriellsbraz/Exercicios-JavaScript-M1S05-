af064206d6916e63795ba4451009b916baabfd12
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-06-26 10:05:58 -0300

    [M1S05] Ex 1 - Escreva um programa para classificação idade

---
+++ exercicio1.js
@@ -0,0 +1,11 @@
+let idade = 18;
+
+if (idade >= 0 && idade <= 12) {
+  console.log("Criança");
+} else if (idade >= 13 && idade <= 17) {
+  console.log("Adolescente");
+} else if (idade >= 18 && idade <= 59) {
+  console.log("Adulto");
+} else {
+  console.log("Idoso");
+}
