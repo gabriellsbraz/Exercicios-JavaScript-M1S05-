2cf189e4ffe135591c38dc827a7c1859e428b056
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-06-26 14:22:56 -0300

    [M1S05] Ex 5 - Escreva um programa que verifique um array de palavras

---
+++ exercicio5.js
@@ -0,0 +1,9 @@
+let stringsArray = ["bola", "caixa", "cartas", "ave", "dinheiro", "chave", "carro", "moto"];
+let stringsArrayClone = [];
+
+for (stringsArray of stringsArrayClone){
+    if(stringsArray.length>=5){
+      stringsArrayClone.push(stringsArray)
+    }
+}
+console.log(stringsArray)
